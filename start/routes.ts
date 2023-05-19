72155063c7faa27000fca0ec4624c82160151aa0
Bima Satria Utama <72238731+BimaUtama8@users.noreply.github.com>
2023-05-19 22:09:12 +0700

    day 2 learn adonis

--- start/routes.ts
+++ start/routes.ts
@@ -5,22 +5,22 @@ Route.get('/', async ({ view }) => {
   return view.render('welcome')
 })
 
-Route.on("/news").render("news.view");
+Route.on("/news").render("news.view").as("news.view");
 
 Route.post('/news', ({ response }) => {
   return response.redirect("/news");
-})
+}).as('news.create');
 
 Route.patch("/news/:id", ({ params }) => {
   return {params};
 }).where('id', {
   match: /^[0-9]+$/,
   cast: (id) => Number(id),
-});
+}).as('news.update');
 
 Route.delete("/news/:id", ({ params }) => {
   return {params};
 }).where('id',{
   match: /^[0-9]+$/,
   cast: (id) => Number(id),
-});
+}).as('news.delete');
