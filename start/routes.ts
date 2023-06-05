9e6822b904a12915d813dcd58ba1139c16afbc9b
Bima Satria Utama <72238731+BimaUtama8@users.noreply.github.com>
2023-06-05 22:19:43 +0700

    templating

--- start/routes.ts
+++ start/routes.ts
@@ -1,10 +1,10 @@
-import { Request, Response } from '@adonisjs/core/build/standalone';
+// import { Request, Response } from '@adonisjs/core/build/standalone';
 import Route from '@ioc:Adonis/Core/Route'
-import Database from '@ioc:Adonis/Lucid/Database';
-import ArtikelsController from 'App/Controllers/Http/ArtikelsController';
+// import Database from '@ioc:Adonis/Lucid/Database';
+// import ArtikelsController from 'App/Controllers/Http/ArtikelsController';
 
 
-Route.on('/').render('welcome').as("welcome");
+Route.on('/').render('auth/login').as("auth.login");
 
 Route.resource("news", "ArtikelsController").paramFor('news', 'slug').middleware({
     edit: ['auth'],
@@ -13,17 +13,17 @@ Route.resource("news", "ArtikelsController").paramFor('news', 'slug').middleware
     destroy: ['auth'],
 });
 
-Route.on('/login').render("auth.login").as("auth.login");
+Route.on('/dashboard').render("home/dashboard").as("home.dashboard");
 
-Route.post('/login', async ({ auth, request, response }) => {
+Route.post('/', async ({ auth, request, response }) => {
     const email = request.input('email')
     const password = request.input('password')
   
     await auth.use('web').attempt(email, password);
-    return response.redirect("/");
+    return response.redirect("/dashboard");
 });
 
 Route.post('/logout', async ({ auth, response }) => {
     await auth.use('web').logout()
-    response.redirect('/login')
+    response.redirect('/')
 }).as("auth.logout");
